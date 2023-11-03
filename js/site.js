8a8721ae1ae7ddec868f7fa7e52e3d4a3121d147
Jlrod3 <112913660+Jlrod3@users.noreply.github.com>
2023-11-02 22:37:45 -0500

    adding js for other checkboxes

--- js/site.js
+++ js/site.js
@@ -6,4 +6,12 @@ jQuery(function($) {
     $("#newsletter").prop("checked", true);
     $("#nbox").html("<p>&#10004;</p>");
   });
+  $("#ubox").on("click", function () {
+    $("#updates").prop("checked", true);
+    $("#ubox").html("<p>&#10004;</p>");
+  });
+  $("#dbox").on("click", function () {
+    $("#dev").prop("checked", true);
+    $("#dbox").html("<p>&#10004;</p>");
+  });
 });
